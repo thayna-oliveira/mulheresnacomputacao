e19ff1271d46b4d832e2161bd06b2a7812ec7904
thayna-oliveira <oliveira.thayna@outlook.com>
2019-06-17 21:09:41 -0300

    Updates

---
+++ precache-manifest.6f85f8dede055c728eb4b8ad4aa6a39e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009159e4fdb85f2cbdd3c84c7dfda124",
+    "url": "/mulheresnacomputacao/index.html"
+  },
+  {
+    "revision": "d45ad82c11908e6503e1",
+    "url": "/mulheresnacomputacao/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mulheresnacomputacao/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d45ad82c11908e6503e1",
+    "url": "/mulheresnacomputacao/static/js/main.028d8be0.chunk.js"
+  },
+  {
+    "revision": "61718a970c6f2fec1109",
+    "url": "/mulheresnacomputacao/static/js/runtime~main.d202cdc3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mulheresnacomputacao/static/media/logo.5d5d9eef.svg"
+  }
+]);
